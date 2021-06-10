4b64b588d69b8ae41ba15fe2477138ccecbb8672
mazinad <mazinadan2@gmail.com>
2021-06-10 23:05:49 +0200

    Added my Portofolio

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if (this.scrollY>20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    var typed=new Typed(".typing",{
+        strings:["Blogger","Designer","Developer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    })
+});
